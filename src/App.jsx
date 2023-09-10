77fd8d7184121544af104b25d279fe935d23e7de
B7Bprog <belabertalan@gmail.com>
2023-09-10 16:22:24 +0100

    set up routing with browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import ToDoList from "./components/TodoList";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<ToDoList />} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-  return <h2>NavBar</h2>;
+  return (
+    <div>
+      <h2>NavBar</h2>
+      <Link to="/">Home</Link>
+      <Link to="/todo">Todo List</Link>
+    </div>
+  );
 };
 
 export default NavBar;
