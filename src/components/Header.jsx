af858484c3a6bdd28cf91f7fb097e19323518c7f
B7Bprog <belabertalan@gmail.com>
2023-09-16 11:48:22 +0100

    style: create style sheets and imports for current components #1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import NavBar from "./NavBar";
-
+import styles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,5 @@
+import styles from "../styles/Home.module.css";
+
 const Home = () => {
   return <h1>Home</h1>;
 };
